1fa5c58a79be2a09fb2e560b9f80b9b99d36644c
Ashley Castillo <afiocastimo02@gmail.com>
2022-06-23 15:46:17 -0600

    Edition of the page
    I finished all the page

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular';
+
+  caracters:Array<string>=["Luffy","Zoro","Chopper"]
+  caracters2:Array<string>=["Ace","Shanks","Nami"]
 }
